458e74d487bd39d2b8f5e3b2289b96522b284fc2
Dankhra sandip <39943347+SandipDankhra@users.noreply.github.com>
2020-02-28 12:52:21 +0530

    Update Employee.cs

--- PGManagement.Models/DbEntities/Main/Employee.cs
+++ PGManagement.Models/DbEntities/Main/Employee.cs
@@ -56,12 +56,12 @@ namespace PGManagement.Models.Main
         [InverseProperty("Employee")]
 		#endregion Expenses Annotations
 
-        public virtual ICollection<Expenses> Expenses { get; set; }
+        public virtual ICollection<Expense> Expenses { get; set; }
 
 
         public Employee()
         {
-			Expenses = new HashSet<Expenses>();
+			Expenses = new HashSet<Expense>();
         }
 	}
 }
